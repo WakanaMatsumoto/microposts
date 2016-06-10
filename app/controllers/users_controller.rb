556dc8e2a247693b11813e3497030a0c7665f33e
wakanamatsumoto <wakana_matsumoto@glocalize.co.jp>
2016-06-10 12:44:31 +0000

    add columns

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,19 @@
 class UsersController < ApplicationController
-  def show
-    @user = User.find(params[:id])
-  end
-  
+  before_action :set_user, only: [ :edit, :update, :destroy ]
+
   def new
     @user = User.new
+  end
+
+  def index
+    @user = User.new
+  end
+
+  def edit
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
   def create
@@ -16,10 +25,22 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user, notice: 'プロフィールを更新しました'
+    else
+      render 'edit'
+    end
+  end
 
   private
   def user_params
-    params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :location, :profile)
   end
 end
