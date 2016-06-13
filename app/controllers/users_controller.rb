2c54f448dc5d7c478b5abec1b7d1cfb8b9e9e91c
wakanamatsumoto <wakana_matsumoto@glocalize.co.jp>
2016-06-13 13:18:14 +0000

    add Validation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,14 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [ :edit, :update, :destroy ]
+  before_action :set_user, only: [:show, :edit, :update]
+  bofore_action :correct_user, only: [:edit, :update]
+  
+  def show
+  end
 
   def new
     @user = User.new
   end
-
-  def index
-    @user = User.new
-  end
-
-  def edit
-  end
-
-  def show
-    @user = User.find(params[:id])
-  end
-
+  
   def create
     @user = User.new(user_params)
     if @user.save
@@ -25,11 +18,9 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
-  
-  def set_user
-    @user = User.find(params[:id])
-  end
 
+  def edit
+  end
 
   def update
     if @user.update(user_params)
@@ -40,7 +31,16 @@ class UsersController < ApplicationController
   end
 
   private
+
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation, :location, :profile)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+  #userをチェックして本人ではない場合rootに飛ばす
+  def correct_user
+    redirect_to root_path if @user != current_user
   end
 end
