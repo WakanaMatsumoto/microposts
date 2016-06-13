37ca4aa5cc6c8f7bcbbe0a069a3f8391c4ab5526
wakanamatsumoto <wakana_matsumoto@glocalize.co.jp>
2016-06-13 13:30:37 +0000

    微修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
-  bofore_action :correct_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
   
   def show
   end
