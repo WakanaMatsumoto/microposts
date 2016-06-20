b208c0aaa8c5802b8fc3f1cb43ad62e5a312826b
wakanamatsumoto <wakana_matsumoto@glocalize.co.jp>
2016-06-20 15:34:13 +0000

    follow lists add

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,6 +1,11 @@
 class RelationshipsController < ApplicationController
   before_action :logged_in_user
 
+  def show
+    @user = User.find(params[:followed_id])
+  end
+
+
   def create
     @user = User.find(params[:followed_id])
     current_user.follow(@user)
